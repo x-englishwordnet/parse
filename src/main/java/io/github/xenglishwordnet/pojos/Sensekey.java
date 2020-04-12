294a663456dc1dfac5c9b6d990c8d4596aaec262
bbou <1313ou@gmail.com>
2020-04-12 09:02:15 +0200

    sensekey decoding

--- src/main/java/io/github/xenglishwordnet/pojos/Sensekey.java
+++ src/main/java/io/github/xenglishwordnet/pojos/Sensekey.java
@@ -1,5 +1,7 @@
 package io.github.xenglishwordnet.pojos;
 
+import java.util.regex.Pattern;
+
 /**
  * Sensekey
  *
@@ -17,9 +19,13 @@ public class Sensekey
 
 	private final int lexId;
 
+	private final NormalizedString headWord; // or null
+
+	private final int headLexId; // -1 for none
+
 	/**
 	 * Copy constructor
-	 * 
+	 *
 	 * @param other
 	 *            other sensekey
 	 */
@@ -30,11 +36,13 @@ public class Sensekey
 		this.pos = other.pos;
 		this.lexDomain = other.lexDomain;
 		this.lexId = other.lexId;
+		this.headWord = other.headWord;
+		this.headLexId = other.headLexId;
 	}
 
 	/**
 	 * Constructor
-	 * 
+	 *
 	 * @param word
 	 *            word (may contain uppercase)
 	 * @param pos
@@ -43,21 +51,28 @@ public class Sensekey
 	 *            lex domain
 	 * @param lexId
 	 *            lexid
+	 * @param headWord
+	 *            head word
+	 * @param headLexId
+	 *            head lexid
 	 * @param key
 	 *            sensekey
 	 */
-	private Sensekey(final NormalizedString word, final Pos pos, final LexDomain lexDomain, final int lexId, final String key)
+	private Sensekey(final NormalizedString word, final Pos pos, final LexDomain lexDomain, final int lexId, final NormalizedString headWord,
+			final int headLexId, final String key)
 	{
 		this.key = key.trim();
 		this.word = word;
 		this.pos = pos;
 		this.lexDomain = lexDomain;
 		this.lexId = lexId;
+		this.headWord = headWord;
+		this.headLexId = headLexId;
 	}
 
 	/**
 	 * Parse sensekey from string
-	 * 
+	 *
 	 * @param str
 	 *            string
 	 * @return sensekey
@@ -68,21 +83,58 @@ public class Sensekey
 	{
 		if (str == null)
 			return null;
-		final String[] fields = str.split("([%:])");
+		final String[] fields = decode(str);
 		if (fields.length < 4)
 			throw new ParsePojoException("Sensekey:" + str);
 		try
 		{
-			final NormalizedString word = new NormalizedString(fields[0].replace('#', ':'));
+			final NormalizedString word = new NormalizedString(fields[0]);
 			final Pos pos = Pos.fromIndex(Integer.parseInt(fields[1]));
 			final LexDomain lexDomain = LexDomain.parseLexDomain(fields[2]);
 			final int lexid = Integer.parseInt(fields[3]);
-			return new Sensekey(word, pos, lexDomain, lexid, str);
+			final NormalizedString headWord = fields[4].isEmpty() ? null : new NormalizedString(fields[4]);
+			final int headLexid = fields[5].isEmpty() ? -1 : Integer.parseInt(fields[5]);
+			return new Sensekey(word, pos, lexDomain, lexid, headWord, headLexid, str);
 		}
 		catch (Exception e)
 		{
 			throw new ParsePojoException("Sensekey:" + str);
 		}
+	}
+
+	//public static final String ESCAPED_PERCENT = "\\%";
+	public static final String ESCAPED_PERCENT = "/";
+
+	/**
+	 * Decode sensekey string into fields
+	 *
+	 * @param skStr
+	 *            sensekey string
+	 * @return fields
+	 * @return fields[0] lemma (not space-normalized)
+	 * @return fields[1] pos
+	 * @return fields[2] lexfile
+	 * @return fields[3] lexid
+	 * @return fields[4] head lemma (or "", not space-normalized)
+	 * @return fields[5] head lexid (or "")
+	 */
+	public static String[] decode(String skStr)
+	{
+		String[] fields = new String[6];
+		String[] fields1 = patternBreak.split(skStr);
+		assert fields1.length == 2;
+		// lemma
+		fields[0] = fields1[0].replace(ESCAPED_PERCENT, "%");
+		// lexsense
+		String lexSense = fields1[1].replace(ESCAPED_PERCENT, "%");
+		// String typeFileLexid = lexSense.substring(0, 8);
+		fields[1] = lexSense.substring(0, 1); // pos
+		fields[2] = lexSense.substring(2, 4); // lexfile
+		fields[3] = lexSense.substring(5, 7); // lexid
+		int last = lexSense.lastIndexOf(':');
+		fields[4] = lexSense.substring(8, last); // head (or "")
+		fields[5] = lexSense.substring(last + 1); // head lexid (or "")
+		return fields;
 	}
 
 	public LexDomain getLexDomain()
@@ -115,6 +167,16 @@ public class Sensekey
 		return this.key;
 	}
 
+	public NormalizedString getHeadWord()
+	{
+		return this.headWord;
+	}
+
+	public int getHeadLexId()
+	{
+		return this.headLexId;
+	}
+
 	@Override
 	public String toString()
 	{
@@ -125,4 +187,6 @@ public class Sensekey
 	{
 		return "word=" + this.word + " lexid=" + this.lexId + " lexdomain=" + this.lexDomain + " pos=" + this.pos;
 	}
+
+	static private Pattern patternBreak = Pattern.compile("(?<!\\u005C)%");
 }
